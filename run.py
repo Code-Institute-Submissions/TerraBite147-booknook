73871f3298f73699a52ae4663f5f9b8b9032b3cf
RLCarlow <logan.carlow@gmail.com>
2023-10-23 19:29:55 +0100

    feat(run): Add initial data structure for library book list

---
+++ run.py
@@ -0,0 +1,21 @@
+# Data structure for the library
+library = [
+    {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "read": True,
+        "rating": 5,
+    },
+    {
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "read": False,
+        "rating": None,
+    },
+        {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "read": False,
+        "rating": None,
+    },
+]
