942d16f2238ba2c07a417e2d85f0efd45cacaedf
RLCarlow <logan.carlow@gmail.com>
2023-10-25 21:22:33 +0100

    feat(run): adjust view_library func to utilize menu function

--- run.py
+++ run.py
@@ -28,37 +28,26 @@ def clear_screen():
 
 
 def prompt_choice(options):
+    """Prompts user to choose from a list of options."""
     for idx, option in enumerate(options, 1):
         print(f"{idx}. {option}")
     return input("\nEnter your choice: ")
 
-
-def view_library():
-    clear_screen()
-    if not library:
-        print("The library is empty.")
-    else:
-        for book in library:
-            print(book.display())
-
-    options = ["Sort", "Add", "Remove", "Return to main menu"]
-    choice = int(prompt_choice(options))
-    library_menu_functions[choice - 1]()
-
-
 def sort_library():
+    """Sorts the library by title, author, read status, or rating."""
     options = ["Sort by title", "Sort by author", "Sort by read status", "Sort by rating", "Return to main menu"]
     choice = int(prompt_choice(options))
     sort_criteria = ["title", "author", "read", "rating"]
 
     if choice in [1, 2, 3, 4]:
         library.sort(key=lambda x: getattr(x, sort_criteria[choice - 1]))
-        view_library()
+        view_library(library)
     elif choice == 5:
-        main_menu()
+        main_menu(library, view_library)
 
 
 def add_book():
+    """Adds a book to the library."""
     title = input("Enter the title of the book: ").strip()
     author = input("Enter the author of the book: ").strip()
 
@@ -87,6 +76,7 @@ def add_book():
 
 
 def remove_book():
+    """Removes a book from the library."""
     title = input("Enter the title of the book you want to remove: ").strip()
     book_to_remove = next((book for book in library if book.title.lower() == title.lower()), None)
 
@@ -100,6 +90,7 @@ def remove_book():
 
 
 def search_for_book():
+    """Searches for a book by title or author."""
     while True:
         options = ["Search by Title", "Search by Author", "Return to main menu"]
         choice = int(prompt_choice(options))
@@ -123,36 +114,69 @@ def search_for_book():
             return
 
 
-library_menu_functions = [sort_library, add_book, remove_book, main_menu]
+library_menu_functions = [sort_library, add_book, remove_book]
 
 
 def about_library_system():
+    """Displays information about the library system."""
     clear_screen()
     print("The BookNook is a Personal Library Management System that allows you to manage your personal collection of books.")
     options = ["Return to main menu"]
     prompt_choice(options)
 
 
-def main_menu():
+def main_menu(library, view_library_fn):
+    """Displays Main Menu"""
     while True:
-        clear_screen()
-        print("--- Personal Library Management System ---")
-        options = ["View Library", "Search for a Book", "About", "Exit"]
-        choice = int(prompt_choice(options))
+        print("\n--- Personal Library Management System ---")
+        print("1. View Library")
+        print("2. Search for a Book")
+        print("3. About")
+        print("4. Exit")
 
-        if choice == 1:
-            view_library()
-        elif choice == 2:
+        choice = input("Enter your choice: \n")
+
+        if choice == "1":
+            view_library_fn(library)
+        elif choice == "2":
             search_for_book()
-        elif choice == 3:
+        elif choice == "3":
             about_library_system()
-        elif choice == 4:
-            print("\nExiting... Thank you for using the Personal Library Management System!")
+        elif choice == "4":
+            print(
+                "Exiting... Thank you for using the Personal Library Management System!"
+            )
             break
+        else:
+            print("Invalid choice. Please try again.")
+
+def view_library(library):
+    """Displays all books in the library and provides library options."""
+    clear_screen()
+
+    if not library:
+        print("Your library is empty!")
+    else:
+        for book in library:
+            print(book.display())
+    
+    print("\n--- Library Menu ---")
+    print("1. Sort Library")
+    print("2. Add a Book")
+    print("3. Remove a Book")
+    print("4. Return to main menu")
+    
+    choice = int(input("\nEnter your choice: \n")) - 1 # Subtract 1 for 0-based index
+    if 0 <= choice < len(library_menu_functions):
+        library_menu_functions[choice]()
+    elif choice == 3:  # Corresponds to "4. Return to main menu" option
+        main_menu(library, view_library)
+    else:
+        print("Invalid choice!")
 
 
 if __name__ == "__main__":
-    main_menu()
+    main_menu(library, view_library)
 
 
 
